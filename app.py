cbd36108fdd1fb400269be5fb86f5051ea1b30a9
ThotaSiddhika <siddhikathota@gmail.com>
2026-07-13 01:10:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -302,33 +302,34 @@ if st.button("🔍 Predict Alzheimer's Risk"):
 
     # Prediction code here
     input_data = pd.DataFrame([[
-        Age, Gender, Ethnicity, EducationLevel, BMI,
-        Smoking, AlcoholConsumption, PhysicalActivity,
-        DietQuality, SleepQuality,
-        FamilyHistoryAlzheimers,
-        CardiovascularDisease,
-        Diabetes,
-        Depression,
-        HeadInjury,
-        Hypertension,
-        SystolicBP,
-        DiastolicBP,
-        CholesterolTotal,
-        CholesterolLDL,
-        CholesterolHDL,
-        CholesterolTriglycerides,
-        MMSE,
-        FunctionalAssessment,
-        MemoryComplaints,
-        BehavioralProblems,
-        ADL,
-        Confusion,
-        Disorientation,
-        PersonalityChanges,
-        DifficultyCompletingTasks,
-        Forgetfulness
-    ]])
-    input_data.columns = [
+    Age, Gender, Ethnicity, EducationLevel, BMI,
+    Smoking, AlcoholConsumption, PhysicalActivity,
+    DietQuality, SleepQuality,
+    FamilyHistoryAlzheimers,
+    CardiovascularDisease,
+    Diabetes,
+    Depression,
+    HeadInjury,
+    Hypertension,
+    SystolicBP,
+    DiastolicBP,
+    CholesterolTotal,
+    CholesterolLDL,
+    CholesterolHDL,
+    CholesterolTriglycerides,
+    MMSE,
+    FunctionalAssessment,
+    MemoryComplaints,
+    BehavioralProblems,
+    ADL,
+    Confusion,
+    Disorientation,
+    PersonalityChanges,
+    DifficultyCompletingTasks,
+    Forgetfulness
+]])
+
+input_data.columns = [
     "Age", "Gender", "Ethnicity", "EducationLevel", "BMI",
     "Smoking", "AlcoholConsumption", "PhysicalActivity",
     "DietQuality", "SleepQuality",
@@ -358,6 +359,7 @@ if st.button("🔍 Predict Alzheimer's Risk"):
 
 st.subheader("Entered Values")
 st.dataframe(input_data)
+
     input_scaled = scaler.transform(input_data)
 
     prediction = model.predict(input_scaled)[0]
