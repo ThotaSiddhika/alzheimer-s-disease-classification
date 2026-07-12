6b92c921afc079dc4ee425ceebd04719cba883a9
ThotaSiddhika <siddhikathota@gmail.com>
2026-07-13 01:28:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     Age = st.number_input("Age", 0, 100, 70)
-    st.caption("Valid range: 0–100:otherwise value is invalid")
+    st.caption("Valid range: 0–100")
 with col2:
     Gender = st.selectbox("Gender", ["Male", "Female"])
     Gender = 1 if Gender == "Male" else 0
@@ -92,7 +92,7 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     BMI = st.number_input("BMI", 10.0, 50.0, 25.0)
-    st.caption("Valid range: 10–50:otherwise value is invalid")
+    st.caption("Valid range: 10–50")
 with col2:
     Smoking = yes_no("Smoking")
 
@@ -103,7 +103,7 @@ with col3:
         20.0,
         5.0
     )
-    st.caption("Valid range: 0–20:otherwise value is invalid")
+    st.caption("Valid range: 0–20")
 PhysicalActivity = st.slider(
     "Physical Activity",
     0.0,
@@ -169,21 +169,21 @@ with col1:
         200,
         120
     )
-    st.caption("Valid range: 80–200:otherwise value is invalid")
+    st.caption("Valid range: 80–200")
     CholesterolTotal = st.number_input(
         "Total Cholesterol",
         100.0,
         400.0,
         200.0
     )
-    st.caption("Valid range: 100–400:otherwise value is invalid")
+    st.caption("Valid range: 100–400")
     CholesterolLDL = st.number_input(
         "LDL Cholesterol",
         20.0,
         300.0,
         100.0
     )
-    st.caption("Valid range: 20–300:otherwise value is invalid")
+    st.caption("Valid range: 20–300")
 with col2:
     DiastolicBP = st.number_input(
         "Diastolic BP",
@@ -191,21 +191,21 @@ with col2:
         150,
         80
     )
-    st.caption("Valid range: 50–150:otherwise value is invalid")
+    st.caption("Valid range: 50–150")
     CholesterolHDL = st.number_input(
         "HDL Cholesterol",
         10.0,
         150.0,
         50.0
     )
-    st.caption("Valid range: 10–150:otherwise value is invalid")
+    st.caption("Valid range: 10–150")
     CholesterolTriglycerides = st.number_input(
         "Triglycerides",
         20.0,
         500.0,
         150.0
     )
-    st.caption("Valid range: 20–500:otherwise value is invalid")
+    st.caption("Valid range: 20–500")
 st.divider()
 
 # -----------------------------------
